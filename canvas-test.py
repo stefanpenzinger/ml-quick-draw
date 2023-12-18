e5476a4f9bf2654541907488743cb654f46b5d0b
Stefan Penzinger <stefan.penzinger13@gmail.com>
2023-12-18 15:11:39 +0100

    Update output of a drawing to be in the correct format

--- canvas-test.py
+++ canvas-test.py
@@ -1,7 +1,7 @@
-import tkinter as tk
-from tkinter import messagebox
 import json
+import tkinter as tk
 from datetime import datetime
+from tkinter import messagebox
 
 from rdp import rdp
 
@@ -14,24 +14,24 @@ root.title(TITLE)
 canvas = tk.Canvas(root, width=CANVAS_SIZE, height=CANVAS_SIZE, bg="white")
 canvas.pack()
 
-inputCoordinates = []
-outputCoordinates = []
+input_coordinates = []
+output_coordinates = []
 stroke_number = 0
 
 
 def prepare_new_stroke(event):
-    inputCoordinates.append([])
-    inputCoordinates[stroke_number].append((event.x, event.y))
+    input_coordinates.append([])
+    input_coordinates[stroke_number].append((event.x, event.y))
 
-    outputCoordinates.append([])
-    outputCoordinates[stroke_number].append([])
-    outputCoordinates[stroke_number].append([])
+    output_coordinates.append([])
+    output_coordinates[stroke_number].append([])
+    output_coordinates[stroke_number].append([])
 
 
 def draw(event):
     color = "black"
     x, y = event.x, event.y
-    inputCoordinates[stroke_number].append((x, y))
+    input_coordinates[stroke_number].append((x, y))
     x1, y1 = x - 1, y - 1
     x2, y2 = x + 1, y + 1
     canvas.create_oval(x1, y1, x2, y2, fill=color, outline=color)
@@ -44,21 +44,23 @@ def update_stroke(event):
 
 
 def save_simplified_stroke():
-    simplified_arr = rdp(inputCoordinates[stroke_number], epsilon=2.0, algo="iter", return_mask=False)
+    simplified_arr = rdp(input_coordinates[stroke_number], epsilon=2.0, algo="iter", return_mask=False)
     print(simplified_arr)
     for i in range(len(simplified_arr)):
         x, y = simplified_arr[i]
-        outputCoordinates[stroke_number][0].append(x)
-        outputCoordinates[stroke_number][1].append(y)
+        output_coordinates[stroke_number][0].append(x)
+        output_coordinates[stroke_number][1].append(y)
 
-    print(outputCoordinates)
+    print(output_coordinates)
 
 
 def save_drawing():
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-    file_name = f"drawings/{timestamp}-picture.json"
+    file_name = f"drawings/{timestamp}-picture.ndjson"
+    output_coordinates_json = {"drawing": output_coordinates}
+
     with open(file_name, "w") as file_stream:
-        json.dump(outputCoordinates, file_stream)
+        json.dump(output_coordinates_json, file_stream)
     msg = messagebox.showinfo(TITLE, "Saved successfully!")
 
 
